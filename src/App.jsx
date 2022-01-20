51304fc6ef445903618dae946d8d2f123d68e183
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-01-20 15:59:05 +0100

    generating images works

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <main class="app__main">
+        <main className="app__main">
         <Meme/>
         </main>
         
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,21 @@
+import memesData from '../data.js'
+import {useState} from 'react'
 export default function Meme(){
+     const [memeImage, setMemeImage] = useState(memesData.data.memes[0].url);
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url);
+        
+    }
     return (
         <div className="meme">
             <div className="meme__input-container">
                 <input type="text" placeholder="Shut up" className="meme__input"/>
                 <input type="text" placeholder="and take my money" className="meme__input" />
             </div>
-            <button className="meme__button">Get a new meme image</button>
+            <button className="meme__button" onClick={getMemeImage}>Get a new meme image</button>
+            <img src={memeImage} alt="" className="meme__image" />
         </div>
     )
 }
